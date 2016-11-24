93cdfea2df6b3377eb6c6c77bad4b1f418719656
myheroo <160133@yses.ac.jp>
2016-11-24 12:00:01 +0900

    4回に1回、”ALOHA!"にした

--- 20161124/Aloha.java
+++ 20161124/Aloha.java
@@ -1,7 +1,12 @@
 public class Aloha{
 	public static void main(String[] args){
 		for(int i=0; i<20; i++){
-			System.out.println("アロハ!");
+			if(i%4==0){
+				System.out.println("ALOHA!");
+			}
+			else{
+				System.out.println("アロハ!");	
+			}
 		}
 	}
 }
