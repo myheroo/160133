0800cd76a7c7cf37681586036e86adceb6d14193
myheroo <160133@yses.ac.jp>
2016-12-12 13:23:35 +0900

    Scanner + while(){..} をつかうのプログラムで、入力が何もなかったら「終わり」

---
+++ 20161212/Aloha.java
@@ -0,0 +1,12 @@
+public class Aloha{
+	public static void main(String[] args){
+		while (true) {
+			String data = new java.util.Scanner(System.in).nextLine();
+			System.out.println(data);
+			if(data.equals("")){
+				break;
+			}
+			
+		}
+	}
+}
