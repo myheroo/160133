e2179efb6522fdf594e50b6465ef456f7d05bf39
myheroo <160133@yses.ac.jp>
2016-12-13 09:35:14 +0900

    While..Scanner()使って,input dataを表示する（繰り返す）

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,7 +1,9 @@
 public class Aloha{
 	public static void main(String[] args){
-		for(int i=0; i<20; i++){
-			System.out.println(args[0]);
+		while(true){
+			System.out.print("input data > ");
+			String data = new java.util.Scanner(System.in).nextLine();
+			System.out.println(data);
 		}
 	}
 }
