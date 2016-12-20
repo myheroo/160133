ce1ffcdc1d0e064b656462ee8e549c1bcdbb4a0a
myheroo <160133@yses.ac.jp>
2016-12-20 11:41:24 +0900

    AlohaWindow.java added.

--- 20161220/AlohaWindow.java
+++ 20161220/AlohaWindow.java
@@ -3,8 +3,22 @@ import javax.swing.*;
 
 public class AlohaWindow {
 	public static void main(String[] args) {
+		//windowを作る
 		JFrame frame = new JFrame();
+		//windowのサイズ
 		frame.setSize(200,200);
+		//windowのタイトル
+		frame.setTitle("オウハク");
+		//windowがを終わるとき、プログラムも終わる
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//"HELLO"と表示するラベル(JLabel)を作る
+		JLabel label = new JLabel("HELLO");
+		Container ctnr = frame.getContentPane();
+		ctnr.add(label,BorderLayout.CENTER);
+		//ボタンを作る
+		JButton button = new JButton("Click Me!");
+		ctnr.add(button,BorderLayout.SOUTH);
+		//windowを表示する
 		frame.setVisible(true);
 	}
 }
