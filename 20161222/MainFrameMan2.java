006e4e285bc18be77a16ea5102c1848e98ae09ce
myheroo <160133@yses.ac.jp>
2016-12-22 11:01:17 +0900

    画像を表示する。

--- 20161222/MainFrameMan2.java
+++ 20161222/MainFrameMan2.java
@@ -1,8 +1,8 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class MainFrameMan {
-	public static void main(String[] args){
+public class MainFrameMan2 {
+	public static void main(String[] args) {
 		FrameMan fm = new FrameMan();
 	}
 }
@@ -12,6 +12,8 @@ class FrameMan {
 	JFrame frame;
 	JPanel panel;
 	JLabel label;
+	ImageIcon icon;
+
 	//メソッド
 	public FrameMan(){
 		System.out.println("ALOHA2!");
@@ -27,9 +29,12 @@ class FrameMan {
 		frame.setSize(300,300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		//画像データを作る
+		icon = new ImageIcon("1_Desktop_Icons/icon_256.png");
+
 		//パネルとラベルを作る
 		panel = new JPanel();
-		label = new JLabel("ALOHA");
+		label = new JLabel(icon);
 
 		//パネルとラベルを表示する
 		panel.add(label);
