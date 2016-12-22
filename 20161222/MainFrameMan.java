72947d193c39f5855ecdfc9c34b52552cdab8bee
myheroo <160133@yses.ac.jp>
2016-12-22 09:55:04 +0900

    MainFrameMan.java added.

---
+++ 20161222/MainFrameMan.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainFrameMan {
+	public static void main(String[] args){
+		FrameMan fm = new FrameMan();
+	}
+}
+
+class FrameMan {
+	//データ
+	JFrame frame;
+
+	//メソッド
+	public FrameMan(){
+		System.out.println("ALOHA2!");
+	}
+}
