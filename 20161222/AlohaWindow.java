fd8465066237040649470bdcdb618c62b03108c9
myheroo <160133@yses.ac.jp>
2016-12-22 09:44:57 +0900

    AlohaWindow.java added.

--- 20161222/AlohaWindow.java
+++ 20161222/AlohaWindow.java
@@ -1,11 +1,15 @@
 import java.awt.*;
-import java.swing.*;
+import javax.swing.*;
 
-public class Aloha {
+public class AlohaWindow {
 	public static void main(String[] args){
 		JFrame frame;
 		//ウィンドウを作る
 		frame = new JFrame();
+		//ウィンドウの設定
+		frame.setLocation(200,200);
+		frame.setSize(300,300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//ウィンドウを表示する
 		frame.setVisible(true);
 	}
